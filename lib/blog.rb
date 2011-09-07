3d11becc2cabb74b5099347ac78314527ec69de0
philip green <pmegreen@yahoo.com>
2011-09-07 17:48:07 -0400

    added initialize url

--- lib/blog.rb
+++ lib/blog.rb
@@ -1,3 +1,16 @@
 #
 # Create your "Blog" class in this file.
 #
+require 'pry'
+
+class Blog
+  def initialize url 
+  
+
+uri = URI.parse("http://rorcourse.tumblr.com/")
+
+# ShortcutNet
+response = Net::HTTP.get_response(uri)
+end
+binding.pry
+
